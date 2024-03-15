f9e01fc9debd095f83ee1af3f6b132db0184e811
Nedymar Schuabe <nedymar.schuabe@agrodanieli.com.br>
2024-03-15 12:04:03 -0300

    CEP que nao contem endereço
    Ajustado para não aparecer os spans com rua, complemente e bairro caso o cep informado não tiver essas informações.

--- React/SujeitoProgramador/Buscador_CEP/buscador/src/App.js
+++ React/SujeitoProgramador/Buscador_CEP/buscador/src/App.js
@@ -48,10 +48,13 @@ function App() {
       {Object.keys(cep).length > 0 && (
         <main className="main">
           <h2>CEP: {cep.cep}</h2>
-
-          <span>{cep.logradouro}</span>
-          <span>Complemento: {cep.complemento}</span>
-          <span>{cep.bairro}</span>
+          {Object.keys(cep.logradouro).length > 0 && (
+            <div className="endereco">
+              <span>{cep.logradouro}</span>
+              <span>Complemento: {cep.complemento}</span>
+              <span>{cep.bairro}</span>
+            </div>
+          )}
           <span>{cep.localidade} - {cep.uf}</span>
         </main>
       )}
