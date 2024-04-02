7fcaf5f40afd1303f9fef49ba8562df0750defcf
nedymarschuabe <nedymar97@hotmail.com>
2024-04-02 09:03:56 -0300

    Aula 4 - Fragments

--- React + Node/HoraDeCodar/meu-projeto/src/App.js
+++ React + Node/HoraDeCodar/meu-projeto/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import SayMayName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
 import Frase from './components/Frase';
+import List from './components/List';
 
 function App() {
 
@@ -18,6 +19,7 @@ function App() {
         profissao="Programador"
         foto="https://via.placeholder.com/150"
       />
+      <List />
     </div>
   );
 }
