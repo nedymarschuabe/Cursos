f790a33af8d1b108333a725e700bb789d6a1b42c
nedymarschuabe <nedymar97@hotmail.com>
2024-04-03 09:10:28 -0300

    Aula 7 - Eventos React

--- React + Node/HoraDeCodar/meu-projeto/src/App.js
+++ React + Node/HoraDeCodar/meu-projeto/src/App.js
@@ -1,25 +1,15 @@
 import './App.css';
-import SayMayName from './components/SayMyName';
-import Pessoa from './components/Pessoa';
-import Frase from './components/Frase';
-import List from './components/List';
+import Evento from './components/Evento';
+import Form from './components/Form';
 
 function App() {
 
   return (
     <div className="App">
       <h1>Testando CSS</h1>
-      <Frase />
-      <Frase />
-      <SayMayName nome="Matheus" />
-      <SayMayName nome="João" />
-      <Pessoa
-        nome="Rodrigo"
-        idade="28"
-        profissao="Programador"
-        foto="https://via.placeholder.com/150"
-      />
-      <List />
+      <Evento numero="1"/>
+      <Evento numero="2"/>
+      <Form />
     </div>
   );
 }
