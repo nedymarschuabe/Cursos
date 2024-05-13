d5c403fe424b7ebe7c53a96dfee5108f3abab20c
nedymarschuabe <nedymar97@hotmail.com>
2024-05-13 15:04:41 -0300

    Aulas Introdução Python

---
+++ DIO/Python AI Backend Developer/aula02/tipos_de_dados.py
@@ -0,0 +1,4 @@
+print(11 + 10 * 2202202) # inteiro --> int()
+print (1.5 + 1 + 0.5) # float --> float()
+print (True) # booleano --> bool()
+print ("Python") # string --> str()
---
+++ DIO/Python AI Backend Developer/aula03/variaveis_constantes.py
@@ -0,0 +1,9 @@
+nome = "Nedymar"
+idade = 27
+
+nome = "Teste"
+print(nome, idade)
+
+BRAZILIAN_STATES =["SP", "RJ", "RS", "SC", "PR"]
+
+print (BRAZILIAN_STATES)
